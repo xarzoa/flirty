27f9ff47df69c5e28ca466740efb562ea99510a2
xar <xarzoa@gmail.com>
2024-08-05 14:08:35 -0700

    final touches

--- bot.ts
+++ bot.ts
@@ -1,6 +1,14 @@
 import { env } from "@/config";
 import { Client, GatewayIntentBits, Partials } from "discord.js";
 import { createResponse } from "@/ai";
+import { serve } from "bun";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
 const client = new Client({
   intents: [
@@ -29,5 +37,10 @@ client.on("messageCreate", async (message) => {
     message.reply(res || "I'm sorry, I couldn't understand you.");
   }
 });
+
+serve({
+  fetch: app.fetch,
+  port: 3000,
+})
 
 client.login(env.BOT_TOKEN);
