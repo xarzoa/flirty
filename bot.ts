464e6e28c29c4b7a8b43eeb3d50d6a6a27ca7d3d
xar <xarzoa@gmail.com>
2024-08-06 13:05:22 -0700

    some ci updates

--- bot.ts
+++ bot.ts
@@ -1,14 +1,6 @@
 import { env } from "@/config";
 import { Client, GatewayIntentBits, Partials } from "discord.js";
 import { createResponse } from "@/ai";
-import { serve } from "bun";
-import { Hono } from "hono";
-
-const app = new Hono();
-
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
 
 const client = new Client({
   intents: [
@@ -33,14 +25,9 @@ client.on("messageCreate", async (message) => {
   if (message.interaction) return;
   if (message.author.bot) return;
   if (message.channel.type === 1) {
-    const res = await createResponse(message.content, parseInt(message.author.id));
+    const res = await createResponse(message.content, Number.parseInt(message.author.id));
     message.reply(res || "I'm sorry, I couldn't understand you.");
   }
 });
-
-serve({
-  fetch: app.fetch,
-  port: 3000,
-})
 
 client.login(env.BOT_TOKEN);
